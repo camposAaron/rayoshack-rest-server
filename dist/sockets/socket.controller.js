0c69afbf64b51b65ad53bbb313921d7b1cecbd2d
Aaron <campos.aaron.15@gmail.com>
2022-01-10 22:21:35 -0600

    git ignore

--- dist/sockets/socket.controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.socketController = void 0;
-//TOWILL: Comentarios de los productos en tiempo real y manipulacion de cesta
-const socketController = (socket) => {
-};
-exports.socketController = socketController;
-//# sourceMappingURL=socket.controller.js.map
